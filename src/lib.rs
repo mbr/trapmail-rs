b925fb9a4a0b16b572b4c6f870dc13685145f5b0
Marc Brinkmann <git@marcbrinkmann.de>
2019-12-09 17:40:25 +0100

    docs: move main docs into library, add link to `docs.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! # trapmail: A sendmail replacement for integration testing
+//!
+//! `trapmail` is a `sendmail` replacement for unit- and integration-testing that captures incoming
+//! mail and stores it on the filesystem. Test cases can inspect the "sent" mails.
+//!
+//! `trapmail`'s commandline aims to mimick the original `sendmail` arguments, commonly also
+//! implemented by other [MTA](https://en.wikipedia.org/wiki/Message_transfer_agent)s like
+//! Exim or Postfix.
+//!
+//! When `trapmail` receives a message, it stores it along with metadata a JSON file in the
+//! directory named in the `TRAPMAIL_STORE` environment variable, falling back to `/tmp` if
+//! not found. Files are named `trapmail_PPID_PID_TIMESTAMP.json`, where `PPID` is the parent
+//! process' PID, `PID` trapmails `PID` at the time of the call and `TIMESTAMP` a microsecond
+//! accurate timestamp.
+//!
+//! ## Concurrency
+//!
+//! While `trapmail` avoids collisions between stored messages, it cannot guarantee that other
+//! test processes/threads that are running simultaneously do not add to the store as well.
+//! Care must be taken to avoid race conditions, especially when clearing the mail store.
+//!
+//! Providing different `TRAPMAIL_STORE` targets allows for namespacing the data. Otherwise,
+//! depending on the usecase, `PID` and `PPID` may aid in filtering.
+//!
+//! ## API
+//!
+//! The `trapmail` crate comes with a command-line application as well as a library. The
+//! library can be used in tests and applications to access all data that `trapmail` writes.
+//!
+//! ## Command-line options
+//!
+//! Currently, `trapmail` does not "support" all the same command-line options that sendmail
+//! supports (all options are ignored, but logged). If you run into issues due to an
+//! unsupported option, feel free to open a PR to get it added.
+
 use failure::Fail;
 use lazy_static::lazy_static;
 use nix::unistd::Pid;
